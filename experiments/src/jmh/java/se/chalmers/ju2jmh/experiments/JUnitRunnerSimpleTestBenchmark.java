e2aba9bc9de094e41064ab0831312f3803a9df06
Niclas Alexandersson <alniniclas@gmail.com>
2019-12-01 07:56:21 +0100

    Switched to more appropriate time unit.

--- experiments/src/jmh/java/se/chalmers/ju2jmh/experiments/JUnitRunnerSimpleTestBenchmark.java
+++ experiments/src/jmh/java/se/chalmers/ju2jmh/experiments/JUnitRunnerSimpleTestBenchmark.java
@@ -14,21 +14,21 @@ public class JUnitRunnerSimpleTestBenchmark {
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public void invokeDirectly() {
         simpleTest.test();
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public void invokeThroughReflection() throws Throwable {
         JUnit4TestInvoker.invoke(testClass, "test");
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public Result invokeThroughJUnitRunner() {
         JUnitCore runner = new JUnitCore();
         return runner.run(Request.method(testClass, "test"));
